2c2f2e615166a0c8fbf5c29599e269b2d004d7ec
Mark Kariuki <kariukimarkg@gmail.com>
2026-05-27 12:48:19 +0300

    fixing the build command

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'categories/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'products/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
